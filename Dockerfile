dfd48c2e57cf35cd6bb495d875deb8f8272aa7c1
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-02-18 21:01:05 +0300

    some changes

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,6 @@ RUN npx --yes nx run ubs-users:build --skip-nx-cache
 FROM node:20.10.0-alpine
 WORKDIR /app
 COPY --from=build /app/dist/apps/ubs-users /app
+COPY --from=build /app/node_modules /app/node_modules
 EXPOSE 3000
 CMD ["node", "/app/main.js"]
--- libs/users-core/src/lib/ubs-users.module.ts
+++ libs/users-core/src/lib/ubs-users.module.ts
@@ -13,7 +13,7 @@ import {
 } from './domain/email-change-request.schema';
 import { EmailChangeRequestService } from './services/email-change-request.service';
 import { UserAdminController } from './web/user-admin.controller';
-import { BackendJwtUtilsExportModule } from '@ubs-platform/';
+import { BackendJwtUtilsExportModule } from '@ubs-platform/users-mona-microservice-helper';
 
 @Module({
   controllers: [UserController, AuthController, UserAdminController],
