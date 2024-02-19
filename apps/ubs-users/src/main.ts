b77aeaaff2b1aa65f0be368e1515f6ac5af4ca75
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-02-19 20:36:04 +0300

    i forgor microservices

--- apps/ubs-users/src/main.ts
+++ apps/ubs-users/src/main.ts
@@ -7,14 +7,23 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { getMicroserviceConnection } from '@ubs-platform/nest-microservice-setup-util';
+import { exec } from 'child_process';
+import { execArgv } from 'process';
 
 async function bootstrap() {
+  exec('wall ' + process.env.NX_KAFKA_PORT);
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice(getMicroserviceConnection(''));
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  await app.startAllMicroservices();
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
