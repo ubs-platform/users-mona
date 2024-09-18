d3d319b6d98d47fec1eccae12cef02fca185be13
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-09-18 21:43:05 +0300

    kafka client

--- libs/users-mona-microservice-helper/src/lib/backend-jwt-utils.module.ts
+++ libs/users-mona-microservice-helper/src/lib/backend-jwt-utils.module.ts
@@ -1,10 +1,10 @@
-import { Module } from "@nestjs/common";
-import { ClientsModule } from "@nestjs/microservices";
-import { BackendJwtUtilsExportModule } from "./backend-jwt-utils-exports.module";
-import { CommunicationHelper } from "./guards/communication-handler";
-import { JwtStrategy } from "./strategies/jwt.strategy";
-import { UserService } from "./service/user.service";
-import { getMicroserviceConnection } from "@ubs-platform/nest-microservice-setup-util";
+import { Module } from '@nestjs/common';
+import { ClientsModule } from '@nestjs/microservices';
+import { BackendJwtUtilsExportModule } from './backend-jwt-utils-exports.module';
+import { CommunicationHelper } from './guards/communication-handler';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UserService } from './service/user.service';
+import { getMicroserviceConnection } from '@ubs-platform/nest-microservice-setup-util';
 // import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
@@ -15,8 +15,8 @@ import { getMicroserviceConnection } from "@ubs-platform/nest-microservice-setup
     ...BackendJwtUtilsExportModule,
     ClientsModule.register([
       {
-        name: "USER_CLIENT",
-        ...getMicroserviceConnection(""),
+        name: 'KAFKA_CLIENT',
+        ...getMicroserviceConnection(''),
       } as any,
     ]),
   ],
--- ubs-mona-release.sh
+++ ubs-mona-release.sh
@@ -1,1 +1,2 @@
-./release-tool.sh ver$VERSION ./libs/roles ./libs/users-common ../umona-parted/nest-microservice-setup-util ./libs/users-mona-microservice-helper .
+# ./release-tool.sh ver$VERSION ./libs/roles ./libs/users-common ../umona-parted/nest-microservice-setup-util ./libs/users-mona-microservice-helper .
+./release-tool.sh ver3.0.0-beta ./libs/roles ./libs/users-common ../umona-parted/nest-microservice-setup-util ./libs/users-mona-microservice-helper .
