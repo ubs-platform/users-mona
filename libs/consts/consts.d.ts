7204cad4c5c7462480e70f6dc753af0ded160190
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-09-19 00:43:50 +0300

    Version upgraded to 3.0.3-beta

---
+++ libs/consts/consts.d.ts
@@ -0,0 +1,4 @@
+export declare const INTERNAL_COMMUNICATION: {
+    port: number;
+    host: string;
+};
---
+++ libs/consts/consts.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.INTERNAL_COMMUNICATION = void 0;
+exports.INTERNAL_COMMUNICATION = {
+    port: parseInt(process.env.U_USERS_MONA_INTERNAL_COM_PORT),
+    host: process.env.U_USERS_MONA_INTERNAL_COM_HOST,
+};
+//# sourceMappingURL=consts.js.map
