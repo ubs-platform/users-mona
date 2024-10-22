6529d2229124396b4fa43b1777cb39fe0732406b
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-10-22 23:26:36 +0300

    unredundant removals

--- libs/users-common/src/lib/dto/entity-ownership-dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserCapabilityDTO = void 0;
-class UserCapabilityDTO {
-}
-exports.UserCapabilityDTO = UserCapabilityDTO;
-//# sourceMappingURL=entity-ownership-dto.js.map
