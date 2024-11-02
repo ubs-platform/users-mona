19d164d56b8e44a2f33fd06bf964ecc219d7e2fe
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-11-02 18:56:14 +0300

    deleting definition files

--- libs/users-common/src/lib/dto/password-change-dto.d.ts
+++
@@ -1,4 +0,0 @@
-export interface PasswordChangeDto {
-    currentPassword: string;
-    newPassword: string;
-}
--- libs/users-common/src/lib/dto/user-register.model.d.ts
+++
@@ -1,8 +0,0 @@
-export interface UserRegisterDTO {
-    username: string;
-    password: string;
-    primaryEmail: string;
-    name: string;
-    surname: string;
-    localeCode: string;
-}
