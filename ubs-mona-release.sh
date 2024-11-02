a8d779ea064651d7c8d399b790e65b3b559c9a84
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-11-02 21:18:15 +0300

    Version upgraded to 3.2.0

--- ubs-mona-release.sh
+++ ubs-mona-release.sh
@@ -1,2 +1,2 @@
 # ./release-tool.sh ver$VERSION ./libs/roles ./libs/users-common ../umona-parted/nest-microservice-setup-util ./libs/users-mona-microservice-helper .
-./release-tool.sh ver3.1.0 ./libs/roles ./libs/users-common ../umona-parted/nest-microservice-setup-util ./libs/users-mona-microservice-helper .
+./release-tool.sh ver3.2.0 ./libs/roles ./libs/users-common ../umona-parted/nest-microservice-setup-util ./libs/users-mona-microservice-helper .
